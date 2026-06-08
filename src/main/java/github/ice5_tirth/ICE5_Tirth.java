cf92a1738a0c04eccf59d47c27ce7125ee8a007b
Tirth <thaktirt@sheridancollege.ca>
2026-06-08 15:58:37 -0400

    Add workflow files

---
+++ src/main/java/github/ice5_tirth/ICE5_Tirth.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package github.ice5_tirth;
+
+/**
+ *
+ * @author tirth
+ */
+public class ICE5_Tirth {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
